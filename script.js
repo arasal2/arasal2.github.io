f747beb9c8f00079f1d8c46ad5da865bbfead773
arasal2 <113080362+arasal2@users.noreply.github.com>
2024-04-04 01:42:14 -0400

    Updated html and Javascript

--- script.js
+++ script.js
@@ -1,18 +1,63 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+//Nav Navigation
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll('.connection div a');
+    navLinks.forEach(function(navLink) {
+      navLink.addEventListener('click', function(event) {
+        navLinks.forEach(link => link.classList.remove('navBar-font-weight'));
+        this.classList.add('navBar-font-weight');
+        
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+          window.scrollTo({
+            top: targetSection.offsetTop - 105, // Adjust as needed
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
+});
+//default class to About weight
+document.addEventListener("DOMContentLoaded", function() {
+  // Add 'active' class to the "About" navigation link
+  const aboutNavLink = document.querySelector('.about a');
+  aboutNavLink.classList.add('navBar-font-weight');
+});
+  
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
+//expereince selectc hange applle class
+document.addEventListener("DOMContentLoaded", function() {
+const sofiDiv = document.getElementById('sofi-select');
+sofiDiv.classList.add('applle');
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+const parentDiv = document.querySelector('.applle-parent');
+parentDiv.querySelectorAll('img').forEach(img => {
+    img.style.display = 'none';
+});
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    console.log(navbar);
-//   if (window.pageYOffset > sticky && !navbar.classList.contains("sticky")) {
-//     navbar.classList.add("sticky");
-//   } else if(window.pageYOffset == sticky){
-//     navbar.classList.remove("sticky");
-//   }
+const svg = sofiDiv.querySelector('img');
+if (svg) {
+    svg.style.display = 'block';
 }
+
+const divs = parentDiv.querySelectorAll('div');
+divs.forEach(div => {
+    div.addEventListener('click', function() {
+    divs.forEach(d => {
+        d.classList.remove('applle');
+    });
+    
+    this.classList.add('applle');
+    
+    parentDiv.querySelectorAll('img').forEach(img => {
+        img.style.display = 'none';
+    });
+
+    const svg = this.querySelector('img');
+    if (svg) {
+        svg.style.display = 'block';
+    }
+    });
+});
+});
